9ff6c7b8a28e9f657750771b5e77c469787c728f
faportero94 <fabianportero94@gmail.com>
2021-03-14 22:30:08 -0500

    Carga de proyecto

---
+++ Source/EndlessRunner2/Private/MixedEmptyClass.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MixedEmptyClass.h"
+
+MixedEmptyClass::MixedEmptyClass()
+{
+}
+
+MixedEmptyClass::~MixedEmptyClass()
+{
+}
